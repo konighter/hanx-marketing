7773e72760f5abfc8f3b93a04e24c98a92419d90
eric <konighter@gmail.com>
2026-05-11 09:21:00 +0800

    [fix] 依赖问题

--- hzapp-module-hanx-ai/src/main/java/com/hzltd/module/erplus/ai/mas/llm/LlmProvider.java
+++ hzapp-module-hanx-ai/src/main/java/com/hzltd/module/erplus/ai/mas/llm/LlmProvider.java
@@ -1,15 +1,15 @@
 package com.hzltd.module.erplus.ai.mas.llm;
 
 import com.google.adk.models.BaseLlm;
-import com.google.adk.models.OllamaLlm;
+import com.google.adk.models.Gemini;
 
 public class LlmProvider {
 
-    private static final BaseLlm DEFAULT_LLM = new OllamaLlm("qwen3.5:9b", "http://192.168.1.3:11434/v1");
+    //TODO-- 切换模型
+    private static final BaseLlm DEFAULT_LLM = new Gemini("", "");
 
     public static BaseLlm defaultLlm() {
         return DEFAULT_LLM;
     }
-
 
 }
